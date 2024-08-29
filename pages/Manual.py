65362a191d744808e35cf8a7994995bf4f25f04d
PedroManuelJM <mpedrojurado@gmail.com>
2024-08-29 18:48:27 -0500

    update 1

--- pages/Manual.py
+++ pages/Manual.py
@@ -6,7 +6,7 @@ st.title("Manual de uso")
 st.write("Se carga una imagen y se extrae los datos correspondientes como: feha emisión, datos del cliente, datos de los productos (cantidad , unidad medida, descripción,valor unitario, precio total,monto total e IGV)")
 
 # Cargar la imagen desde un archivo local
-img = Image.open("ejemplo.png")
+img = Image.open("https://github.com/PedroManuelJM/image_text_factura/blob/main/images/ejemplo.png")
 
 # Mostrar la imagen
 st.image(img, caption="Descripción de la imagen", use_column_width=True)
